42adc2611bfab6b959b36c1e490168d13846949a
Murat Bastas <muratbsts@gmail.com>
2019-05-06 23:35:14 +0300

    Responsive style

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,31 @@ import "phoenix_html";
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
+(function() {
+  function e(e) {
+    for (r = 0; r < n.length; r++) n[r].classList.remove("popover-open");
+  }
+  function t(t) {
+    t.preventDefault(),
+      document
+        .querySelector(this.getAttribute("href"))
+        .classList.contains("popover-open")
+        ? document
+            .querySelector(this.getAttribute("href"))
+            .classList.remove("popover-open")
+        : (e(),
+          document
+            .querySelector(this.getAttribute("href"))
+            .classList.add("popover-open")),
+      t.stopImmediatePropagation();
+  }
+  var o = document.querySelectorAll("[data-popover]"),
+    n = document.querySelectorAll(".popover"),
+    r = void 0;
+  for (r = 0; r < o.length; r++) o[r].addEventListener("click", t);
+  document.addEventListener("click", e);
+})();
+
 document.addEventListener("DOMContentLoaded", () => {
   const addProfileEl = document.getElementById("add-profile");
 
